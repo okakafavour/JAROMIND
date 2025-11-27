75cd656b71b1ae3c87d4ea181b3f92d1e2fe41cc
AbaraEmmanuel <emmanuelabara265@gmail.com>
2025-11-27 11:53:03 +0100

    edited routes, enable CORS

--- router/routes.go
+++ router/routes.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/okakafavour/jaromind-backend/controllers"
 	"github.com/okakafavour/jaromind-backend/middleware"
@@ -8,11 +11,21 @@ import (
 
 func RegisterRoutes(router *gin.Engine) {
 
-	//--- Auth routes ---
+	// CORS configuration
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // change * to frontend URL in production
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Public routes
 	router.POST("/register", controllers.RegisterUser)
 	router.POST("/login", controllers.LoginUser)
 
-	// ----- protected routes (JWT required) -----
+	// Protected routes
 	protected := router.Group("/user")
 	protected.Use(middleware.JWTAuthMiddleware())
 	{
